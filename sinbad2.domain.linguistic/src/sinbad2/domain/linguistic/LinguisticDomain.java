60913477e4402810bda832ba75780020fe63fe2b
Alvaro Labella <brokensword_19@hotmail.com>
2015-05-27 13:37:34 +0200

    Una vez terminadas las valoraciones vamos a comenzar a realizar el plugin sinbad2.doamin.valuations, que nos va a permitir separar los dominios de la valoraciones (definición de las valoraciones en los dominios)

---
+++ sinbad2.domain.linguistic/src/sinbad2/domain/linguistic/LinguisticDomain.java
@@ -0,0 +1,22 @@
+package sinbad2.domain.linguistic;
+
+import sinbad2.domain.Domain;
+
+public class LinguisticDomain extends Domain {
+	
+	public static final String ID = "flintstones.domain.linguistic";
+	
+	
+	@Override
+	public double midpoint() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String formatDescriptionDomain() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
