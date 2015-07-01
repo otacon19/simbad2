eb440ef5cec6a906a29855d3b3bd8bb9143aa4e4
Alvaro Labella <brokensword_19@hotmail.com>
2015-07-01 17:54:20 +0200

    Finalizado frameworkstructuring

---
+++ sinbad2.resolutionphase.frameworkstructuring.ui/src/sinbad2/resolutionphase/frameworkstructuring/ui/FrameworkStructuringPerspective.java
@@ -0,0 +1,56 @@
+package sinbad2.resolutionphase.frameworkstructuring.ui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IPerspectiveListener;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+
+import sinbad2.element.ui.view.elements.ElementView;
+import sinbad2.resolutionphase.frameworkstructuring.ui.view.elementassignments.ElementAssignmentsView;
+
+public class FrameworkStructuringPerspective implements IPerspectiveFactory {
+	
+	public static final String ID = "flintstones.resolutionphase.frameworkstructuring.perspective";
+	
+	private ElementView _elementView;
+	private ElementAssignmentsView _elementAssignmentsView = null;
+	
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+		
+		PlatformUI.getWorkbench().getActiveWorkbenchWindow().addPerspectiveListener(new IPerspectiveListener() {
+			
+			@Override
+			public void perspectiveChanged(IWorkbenchPage page,IPerspectiveDescriptor perspective, String changeId) {
+				
+				if(ID.equals(perspective.getId())) {
+					if(_elementView == null) {
+						for(IViewReference viewReference: page.getViewReferences()) {
+							if(ElementView.ID.equals(viewReference.getId())) {
+								_elementView = (ElementView) viewReference.getView(false);
+							}
+						}
+					}
+					if(_elementAssignmentsView == null) {
+						for(IViewReference viewReference: page.getViewReferences()) {
+							if(ElementAssignmentsView.ID.equals(viewReference.getId())) {
+								_elementAssignmentsView = (ElementAssignmentsView) viewReference.getView(false);
+							}
+						}
+					}
+				}
+				
+			}
+			
+			@Override
+			public void perspectiveActivated(IWorkbenchPage page,IPerspectiveDescriptor perspective) {}
+		});
+		
+	}
+
+}
--- sinbad2.resolutionphase.frameworkstructuring.ui/src/sinbad2/resolutionphase/frameworkstructuring/ui/view/elementassignments/ElementAssignmentsView.java
+++ sinbad2.resolutionphase.frameworkstructuring.ui/src/sinbad2/resolutionphase/frameworkstructuring/ui/view/elementassignments/ElementAssignmentsView.java
@@ -1,21 +1,35 @@
 package sinbad2.resolutionphase.frameworkstructuring.ui.view.elementassignments;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.contexts.IContextActivation;
 import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.part.ViewPart;
 
 import sinbad2.element.ProblemElement;
 import sinbad2.element.ProblemElementsManager;
 import sinbad2.element.ProblemElementsSet;
+import sinbad2.element.alternative.Alternative;
 import sinbad2.element.alternative.listener.AlternativesChangeEvent;
 import sinbad2.element.alternative.listener.IAlternativesChangeListener;
+import sinbad2.element.criterion.Criterion;
 import sinbad2.element.criterion.listener.CriteriaChangeEvent;
 import sinbad2.element.criterion.listener.ICriteriaChangeListener;
+import sinbad2.element.expert.Expert;
 import sinbad2.element.expert.listener.ExpertsChangeEvent;
 import sinbad2.element.expert.listener.IExpertsChangeListener;
 import sinbad2.element.ui.view.elements.ElementView;
@@ -38,41 +52,347 @@ public class ElementAssignmentsView extends ViewPart implements ISelectionListen
 	private static final IContextService _contextService = (IContextService) PlatformUI.getWorkbench().getService(IContextService.class);
 	
 	@Override
-	public void notifyAlternativesChange(AlternativesChangeEvent event) {
-		// TODO Auto-generated method stub
+	public void createPartControl(Composite parent) {
+		_instance = this;
+		_partName = getPartName();
+		_container = parent;
+		_container.setLayout(new FillLayout());
 		
+		hookFocusListener();
+		
+		getSite().getPage().addSelectionListener(this);
+		setModel();
+		
+		_elementsManager = ProblemElementsManager.getInstance();
+		_elementSet = _elementsManager.getActiveElementSet();
+		_elementSet.registerExpertsChangesListener(this);
+		_elementSet.registerAlternativesChangesListener(this);
+		_elementSet.registerCriteriaChangesListener(this);
 	}
 
+	@Override
+	public void notifyAlternativesChange(AlternativesChangeEvent event) {
+		
+		switch (event.getChange()) {
+			case MODIFY_ALTERNATIVE:
+				if (_selectedElement == event.getNewValue()) {
+					setModel();
+				}
+				break;
+			default:
+		}
+	}
+
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void notifyCriteriaChange(CriteriaChangeEvent event) {
-		// TODO Auto-generated method stub
+		Criterion criterion;
+		List<Criterion> criteria;
+		
+		switch (event.getChange()) {
+			case ADD_CRITERION:
+				criterion = (Criterion) event.getNewValue();
+				while (criterion.getParent() != null) {
+					criterion = criterion.getParent();
+					if (_selectedElement == criterion) {
+						setModel();
+					}
+				}
+				break;
+			case REMOVE_CRITERION:
+				criterion = (Criterion) event.getOldValue();
+				while (criterion.getParent() != null) {
+					criterion = criterion.getParent();
+					if (_selectedElement == criterion) {
+						setModel();
+					}
+				}
+				break;
+			case ADD_CRITERIA:
+				criteria = (List<Criterion>) event.getNewValue();
+				for (Criterion c : criteria) {
+					while (c.getParent() != null) {
+						c = c.getParent();
+						if (_selectedElement == c) {
+							setModel();
+						}
+					}
+				}
+				break;
+			case REMOVE_CRITERIA:
+				criteria = (List<Criterion>) event.getOldValue();
+				for (Criterion c : criteria) {
+					while (c.getParent() != null) {
+						c = c.getParent();
+						if (_selectedElement == c) {
+							setModel();
+						}
+					}
+				}
+				break;
+			case MOVE_CRITERION:
+				criterion = (Criterion) event.getNewValue();
+				while (criterion.getParent() != null) {
+					criterion = criterion.getParent();
+					if (_selectedElement == criterion) {
+						setModel();
+					}
+				}
+				criterion = (Criterion) event.getOldValue();
+				if (criterion != null) {
+					while (criterion.getParent() != null) {
+						criterion = criterion.getParent();
+						if (_selectedElement == criterion) {
+							setModel();
+						}
+					}
+				}
+				break;
+			case MODIFY_CRITERION:
+				criterion = (Criterion) event.getNewValue();
+				while (criterion.getParent() != null) {
+					criterion = criterion.getParent();
+					if (_selectedElement == criterion) {
+						setModel();
+					}
+				}
+				break;
+	
+			default:
+		}
+
+
 		
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void notifyExpertsChange(ExpertsChangeEvent event) {
-		// TODO Auto-generated method stub
+		Expert expert;
+		List<Expert> experts;
+		
+		switch (event.getChange()) {
+			case ADD_EXPERT:
+				expert = (Expert) event.getNewValue();
+				while (expert.getParent() != null) {
+					expert = expert.getParent();
+					if (_selectedElement == expert) {
+						setModel();
+					}
+				}
+				break;
+			case REMOVE_EXPERT:
+				expert = (Expert) event.getOldValue();
+				while (expert.getParent() != null) {
+					expert = expert.getParent();
+					if (_selectedElement == expert) {
+						setModel();
+					}
+				}
+				break;
+			case ADD_MULTIPLE_EXPERTS:
+				experts = (List<Expert>) event.getNewValue();
+				for (Expert e : experts) {
+					while (e.getParent() != null) {
+						e = e.getParent();
+						if (_selectedElement == e) {
+							setModel();
+						}
+					}
+				}
+				break;
+			case REMOVE_MULTIPLE_EXPERTS:
+				experts = (List<Expert>) event.getOldValue();
+				for (Expert e : experts) {
+					while (e.getParent() != null) {
+						e = e.getParent();
+						if (_selectedElement == e) {
+							setModel();
+						}
+					}
+				}
+				break;
+			case MOVE_EXPERT:
+				expert = (Expert) event.getNewValue();
+				while (expert.getParent() != null) {
+					expert = expert.getParent();
+					if (_selectedElement == expert) {
+						setModel();
+					}
+				}
+				expert = (Expert) event.getOldValue();
+				if (expert != null) {
+					while (expert.getParent() != null) {
+						expert = expert.getParent();
+						if (_selectedElement == expert) {
+							setModel();
+						}
+					}
+				}
+				break;
+			case MODIFY_EXPERT:
+				expert = (Expert) event.getNewValue();
+				while (expert.getParent() != null) {
+					expert = expert.getParent();
+					if (_selectedElement == expert) {
+						setModel();
+					}
+				}
+	
+			default:
+		}
+
+
 		
 	}
 
 	@Override
 	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
+		if(part instanceof ElementView) {
+			setElementSelected((StructuredSelection) selection);
+			setModel();
+		}
 		
 	}
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
+		_container.setFocus();
+		
+		if(_selectedElement == null) {
+			forceSelection();
+		}	
+	}
+	
+	@Override
+	public void dispose() {
+		
+		if(_tabFolder != null) {
+			if(!_tabFolder.isDisposed()) {
+				for(TabItem item: _tabFolder.getItems()) {
+					item.getControl().dispose();
+				}
+			}
+			_tabFolder.dispose();
+			_tabFolder = null;
+		}
+		getSite().getPage().removeSelectionListener(this);
+		_elementSet.unregisterExpertsChangeListener(this);
+		_elementSet.unregisterCriteriaChangeListener(this);
+		_elementSet.unregisterAlternativesChangeListener(this);
+		super.dispose();
+	}
+	
+	private void setModel() {
+		
+		if(_tabFolder != null) {
+			for(TabItem item: _tabFolder.getItems()) {
+				item.getControl().dispose();
+			}
+			_tabFolder.dispose();
+			_tabFolder = null;
+		}
+		
+		if(_selectedElement != null) {
+			List<ProblemElement> toDisplay = new LinkedList<ProblemElement>();
+			if(_selectedElement instanceof Expert) {
+				List<Expert> nextLevelExperts = new LinkedList<Expert>();
+				nextLevelExperts.add((Expert) _selectedElement);
+				do {
+					List<Expert> aux = new LinkedList<Expert>(nextLevelExperts);
+					nextLevelExperts = new LinkedList<Expert>();
+					for(Expert expert: aux) {
+						if(expert.hasChildrens()) {
+							nextLevelExperts.addAll(expert.getChildrens());
+						} else {
+							toDisplay.add(expert);
+						}
+					}
+				} while(!nextLevelExperts.isEmpty());
+			} else if(_selectedElement instanceof Alternative) {
+				toDisplay.add(_selectedElement);
+			} else if(_selectedElement instanceof Criterion) {
+				List<Criterion> nextLevelCriteria = new LinkedList<Criterion>();
+				nextLevelCriteria.add((Criterion) _selectedElement);
+				do {
+					List<Criterion> aux = new LinkedList<Criterion>(nextLevelCriteria);
+					nextLevelCriteria = new LinkedList<Criterion>();
+					for(Criterion criterion: aux) {
+						if(criterion.hasSubcriteria()) {
+							nextLevelCriteria.addAll(criterion.getSubcriteria());
+						} else {
+							toDisplay.add(criterion);
+						}
+					}
+				} while(!nextLevelCriteria.isEmpty());
+			}
+			_tabFolder = new TabFolder(_container, SWT.BORDER);
+			for(ProblemElement element: toDisplay) {
+				TabItem item = new TabItem(_tabFolder, SWT.NULL);
+				item.setText(element.getPathId());
+				ElementAssignmentsTable elementAssignmentsTable = new ElementAssignmentsTable(_tabFolder);
+				item.setControl(elementAssignmentsTable);
+				elementAssignmentsTable.setModel(element);
+			}
+			if(_selectedElement instanceof Alternative) {
+				_instance.setPartName(_partName +  " | " + "Alternative");
+			} else if(_selectedElement instanceof Criterion) {
+				_instance.setPartName(_partName +  " | " + "Criterion");
+			} else {
+				_instance.setPartName(_partName +  " | " + "Expert");
+			}
+		} else {
+			_instance.setPartName(_partName);
+			forceSelection();
+		}
+	}
+	
+	private void forceSelection() {
+		
+		if(_elementView == null) {
+			for(IViewReference reference: getSite().getPage().getViewReferences()) {
+				if(reference.getId().equals(ElementView.ID)) {
+					_elementView = (ElementView) reference.getView(true);
+				}
+			}
+		} else {
+			StructuredSelection selection = (StructuredSelection) _elementView.getSelection();
+			if(selection != null) {
+				if(!selection.isEmpty()) {
+					setElementSelected(selection);
+					setModel();
+				}
+			}
+		}
 		
 	}
 	
+	private void hookFocusListener() {
+		_container.addFocusListener(new FocusListener() {
+			
+			private IContextActivation activation = null;
+			
+			@Override
+			public void focusLost(FocusEvent e) {
+				_contextService.deactivateContext(activation);	
+			}
+			
+			@Override
+			public void focusGained(FocusEvent e) {
+				activation = _contextService.activateContext(CONTEXT_ID);	
+			}
+		});	
+	}
 	
+	private void setElementSelected(StructuredSelection selection) {
+		ProblemElement element = null;
+		
+		if(!selection.isEmpty()) {
+			element = (ProblemElement) selection.getFirstElement();
+		}
+		_selectedElement = element;
+	}
 
 }
