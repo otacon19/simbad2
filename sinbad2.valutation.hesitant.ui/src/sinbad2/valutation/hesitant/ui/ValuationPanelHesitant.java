912db80916dafbae4ee6888b79787691ba548a8a
Alvaro <brokensword_19@hotmail.com>
2015-07-13 20:51:35 +0200

    Terminar el panel de valoración del hesitant

--- sinbad2.valutation.hesitant.ui/src/sinbad2/valutation/hesitant/ui/ValuationPanelHesitant.java
+++ sinbad2.valutation.hesitant.ui/src/sinbad2/valutation/hesitant/ui/ValuationPanelHesitant.java
@@ -30,38 +30,40 @@ public class ValuationPanelHesitant extends ValuationPanel {
 		GridLayout layout = new GridLayout(2, true);
 		_valuationPart.setLayout(layout);
 		
-		GridData gd = new GridData(SWT.LEFT, SWT.BOTTOM, false, true, 1, 1);
-
-		gd.horizontalIndent = 30;
+		GridData gd = new GridData(SWT.CENTER, SWT.BOTTOM, false, true, 1, 1);
 		
 		_primaryButton = new Button(_valuationPart, SWT.RADIO);
 		_primaryButton.setLayoutData(gd);
+		gd.horizontalIndent = 13;
+		gd.verticalIndent = 10;
 		_primaryButton.setText("Primary");
 		
-		 gd = new GridData(SWT.LEFT, SWT.BOTTOM, false, true, 1, 1);
+		 gd = new GridData(SWT.CENTER, SWT.BOTTOM, false, true, 1, 1);
 		_unaryRelationshipButton = new Button(_valuationPart, SWT.RADIO);
 		_unaryRelationshipButton.setLayoutData(gd);
 		gd.horizontalIndent = 30;
 		_unaryRelationshipButton.setText("Unary");
 		
-		 gd = new GridData(SWT.RIGHT, SWT.BOTTOM, false, false, 1, 1);
+		 gd = new GridData(SWT.CENTER, SWT.TOP, false, true, 1, 1);
 		_compositeButton = new Button(_valuationPart, SWT.RADIO);
 		_compositeButton.setLayoutData(gd);
 		gd.horizontalIndent = 30;
+		gd.verticalIndent = 10;
 		_compositeButton.setText("Composite");
 		
-		gd = new GridData(SWT.LEFT, SWT.BOTTOM, false, false, 1, 1);
+		gd = new GridData(SWT.CENTER, SWT.TOP, false, true, 1, 1);
 		_binaryRelationshipButton = new Button(_valuationPart, SWT.RADIO);
 		_binaryRelationshipButton.setLayoutData(gd);
 		gd.horizontalIndent = 30;
+		gd.verticalIndent = 10;
 		_binaryRelationshipButton.setText("Binary");
 		
+		gd = new GridData(SWT.CENTER, SWT.TOP, false, false, 2, 1);
 		Label label = new Label(_valuationPart, SWT.NONE);
-		label = new Label(_valuationPart, SWT.NONE);
-		label.setLayoutData(new GridData(SWT.CENTER, SWT.BOTTOM, false, true, 2, 1));
+		label.setLayoutData(gd);
+		gd.horizontalIndent = 30;
 		label.setText("Value");
 		label.setBackground(new Color(Display.getCurrent(), 255, 255, 255));
-		new Label(_valuationPart, SWT.NONE);
 		
 		String[] labels = new String[((FuzzySet) _domain).getLabelSet().getCardinality()];
 		
@@ -71,9 +73,10 @@ public class ValuationPanelHesitant extends ValuationPanel {
 			cont++;
 		}
 		
-		new Label(_valuationPart, SWT.NONE);
+		gd = new GridData(SWT.CENTER, SWT.TOP, false, true, 2, 1);
 		_labelCombo = new Combo(_valuationPart, SWT.BORDER);
-		_labelCombo.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, true, 2, 1));
+		_labelCombo.setLayoutData(gd);
+		gd.horizontalIndent = 30;
 		_labelCombo.setBackground(new Color(Display.getCurrent(), 255, 255, 255));
 		_labelCombo.setItems(labels);
 
