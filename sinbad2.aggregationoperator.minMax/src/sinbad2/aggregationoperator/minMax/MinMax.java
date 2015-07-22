4e78d46ccd9e567145ad24e62b104e641c91157f
Alvaro Labella <brokensword_19@hotmail.com>
2015-07-22 10:47:31 +0200

    Operador MaxMin terminado

---
+++ sinbad2.aggregationoperator.minMax/src/sinbad2/aggregationoperator/minMax/MinMax.java
@@ -0,0 +1,16 @@
+package sinbad2.aggregationoperator.minMax;
+
+import java.util.List;
+
+import sinbad2.aggregationoperator.UnweightedAggregationOperator;
+import sinbad2.valuation.Valuation;
+
+public class MinMax extends UnweightedAggregationOperator {
+
+	@Override
+	public Valuation aggregate(List<Valuation> valuations) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ sinbad2.aggregationoperator.minMax/src/sinbad2/aggregationoperator/minMax/valuation/HesitantOperator.java
@@ -0,0 +1,48 @@
+package sinbad2.aggregationoperator.minMax.valuation;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import sinbad2.core.validator.Validator;
+import sinbad2.domain.linguistic.fuzzy.FuzzySet;
+import sinbad2.valuation.Valuation;
+import sinbad2.valuation.hesitant.HesitantValuation;
+
+public class HesitantOperator {
+	
+	private HesitantOperator() {}
+	
+	public static Valuation aggregate(List<Valuation> valuations) {
+		Valuation result = null;
+		FuzzySet domain = null;
+		List<Valuation> values = new LinkedList<Valuation>();
+		List<Integer> max = new LinkedList<Integer>();
+		
+		for(Valuation valuation: valuations) {
+			Validator.notIllegalElementType(valuation, new String[] {HesitantValuation.class.toString()});
+			
+			if(domain == null) {
+				domain = (FuzzySet) valuation.getDomain();
+			} else if(!domain.equals(valuation.getDomain())) {
+				throw new IllegalArgumentException("Invalid domain");
+			}
+			
+			values.add(valuation);
+		}
+		
+		if(!values.isEmpty()) {
+			for(Valuation valuation: values) {
+				if(valuation instanceof HesitantValuation) {
+					max.add(((HesitantValuation) valuation).getEnvelopeIndex()[1]);
+				}
+			}
+			Collections.sort(max);
+			result = new HesitantValuation(domain);
+			((HesitantValuation) result).setLabel(max.get(0));
+		}
+		
+		return result;
+	}
+
+}
