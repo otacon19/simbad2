d9b894abc75b2890da50379defd991355315a7f6
Álvaro Labella Romero <brokensword_19@hotmail.com>
2015-12-14 17:45:32 +0100

    Creado plugin .ui de fase de unificación en problema con
    multigranularidad

---
+++ sinbad2.phasemethod.multigranular.unification.ui/src/sinbad2/phasemethod/multigranular/unification/ui/view/SelectBLTS.java
@@ -0,0 +1,20 @@
+package sinbad2.phasemethod.multigranular.unification.ui.view;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class SelectBLTS extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
