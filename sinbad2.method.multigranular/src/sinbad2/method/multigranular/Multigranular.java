eac6598f86987c5be2d512d16aede81712fb9f7c
Álvaro Labella Romero <brokensword_19@hotmail.com>
2015-12-21 16:51:10 +0100

    Creado plugin de método multigranular

---
+++ sinbad2.method.multigranular/src/sinbad2/method/multigranular/Multigranular.java
@@ -0,0 +1,20 @@
+package sinbad2.method.multigranular;
+
+import sinbad2.method.MethodImplementation;
+import sinbad2.method.state.MethodStateChangeEvent;
+
+public class Multigranular extends MethodImplementation {
+
+	@Override
+	public void notifyMethodStateChange(MethodStateChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MethodImplementation newInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
