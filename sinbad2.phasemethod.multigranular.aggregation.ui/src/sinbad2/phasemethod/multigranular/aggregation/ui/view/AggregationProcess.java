77f44a3b4d93c4523c34c2454029f610777c65c0
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-01-20 17:41:48 +0100

    Creado plugin de interfaz para la fase de agregación del método
    multigranular

---
+++ sinbad2.phasemethod.multigranular.aggregation.ui/src/sinbad2/phasemethod/multigranular/aggregation/ui/view/AggregationProcess.java
@@ -0,0 +1,27 @@
+package sinbad2.phasemethod.multigranular.aggregation.ui.view;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class AggregationProcess extends ViewPart {
+	
+	public static final String ID = "flintstones.phasemethod.multigranular.aggregation.ui.view.aggregationprocess";
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public String getPartName() {
+		return "Aggregation process";
+	}
+
+}
