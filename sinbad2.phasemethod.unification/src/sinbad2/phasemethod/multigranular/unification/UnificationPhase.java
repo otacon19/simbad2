12cf77768701804b52bd33693e4499ecac19cb10
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-01-20 17:30:54 +0100

    Terminado plugin de fase de agregación de método multigranular.

--- sinbad2.phasemethod.unification/src/sinbad2/phasemethod/multigranular/unification/UnificationPhase.java
+++ sinbad2.phasemethod.unification/src/sinbad2/phasemethod/multigranular/unification/UnificationPhase.java
@@ -3,8 +3,6 @@ package sinbad2.phasemethod.multigranular.unification;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import sinbad2.domain.DomainSet;
-import sinbad2.domain.DomainsManager;
 import sinbad2.domain.linguistic.fuzzy.FuzzySet;
 import sinbad2.element.criterion.Criterion;
 import sinbad2.phasemethod.IPhaseMethod;
@@ -20,23 +18,19 @@ import sinbad2.valuation.valuationset.ValuationSetManager;
 
 public class UnificationPhase implements IPhaseMethod {
 
-	public static final String ID = "flintstones.phasemethod.unification";
+	public static final String ID = "flintstones.phasemethod.multigranular.unification";
 
-	private DomainsManager _domainsManager;
-	private DomainSet _domainSet;
-
+	private ValuationSetManager _valuationSetManager;
 	private static ValuationSet _valutationSet;
 
+	
 	public UnificationPhase() {
-		_domainsManager = DomainsManager.getInstance();
-		_domainSet = _domainsManager.getActiveDomainSet();
-
-		ValuationSetManager valuationSetManager = ValuationSetManager.getInstance();
-		_valutationSet = valuationSetManager.getActiveValuationSet();
+		_valuationSetManager = ValuationSetManager.getInstance();
+		_valutationSet = _valuationSetManager.getActiveValuationSet();
 	}
 
-	public DomainSet getDomainSet() {
-		return _domainSet;
+	public ValuationSet getValuationSet() {
+		return _valutationSet;
 	}
 
 	@Override
@@ -50,12 +44,12 @@ public class UnificationPhase implements IPhaseMethod {
 
 		clear();
 
-		_domainSet.setDomains(unification.getDomainSet().getDomains());
+		_valutationSet.setValuations(unification.getValuationSet().getValuations());
 	}
 
 	@Override
 	public void clear() {
-		_domainSet.clear();
+		_valutationSet.clear();
 	}
 
 	@Override
@@ -71,7 +65,6 @@ public class UnificationPhase implements IPhaseMethod {
 
 		try {
 			result = (UnificationPhase) super.clone();
-			result._domainSet = (DomainSet) _domainSet.clone();
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
@@ -81,13 +74,13 @@ public class UnificationPhase implements IPhaseMethod {
 
 	@Override
 	public void activate() {
-		_domainsManager.setActiveDomainSet(_domainSet);
+		_valuationSetManager.setActiveValuationSet(_valutationSet);
 	}
 
 	@Override
 	public boolean validate() {
 
-		if (_domainSet.getDomains().isEmpty()) {
+		if (_valutationSet.getValuations().isEmpty()) {
 			return false;
 		}
 
