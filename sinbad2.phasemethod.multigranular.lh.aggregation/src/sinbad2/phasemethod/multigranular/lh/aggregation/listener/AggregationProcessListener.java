56c2a008c3a291c2e429aa7865ca2f2c223f20fa
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-02-12 10:35:00 +0100

    Completado plugin de agregación de LH

---
+++ sinbad2.phasemethod.multigranular.lh.aggregation/src/sinbad2/phasemethod/multigranular/lh/aggregation/listener/AggregationProcessListener.java
@@ -0,0 +1,7 @@
+package sinbad2.phasemethod.multigranular.lh.aggregation.listener;
+
+public interface AggregationProcessListener {
+	
+	public void notifyAggregationProcessChange(AggregationProcessStateChangeEvent event);
+	
+}
---
+++ sinbad2.phasemethod.multigranular.lh.aggregation/src/sinbad2/phasemethod/multigranular/lh/aggregation/listener/AggregationProcessStateChangeEvent.java
@@ -0,0 +1,35 @@
+package sinbad2.phasemethod.multigranular.lh.aggregation.listener;
+
+public class AggregationProcessStateChangeEvent {
+	
+	private EAggregationProcessStateChange _change;
+	private Object _oldValue;
+	private Object _newValue;
+	
+	public AggregationProcessStateChangeEvent() {
+		_change = null;
+		_oldValue = null;
+		_newValue = null;
+	}
+	
+	public AggregationProcessStateChangeEvent(EAggregationProcessStateChange change, Object oldValue, Object newValue) {
+		this();
+		_change = change;
+		_oldValue = oldValue;
+		_newValue = newValue;
+	}
+	
+	public EAggregationProcessStateChange getChange() {
+		return _change;
+	}
+	
+	public Object getOldValue() {
+		return _oldValue;
+	}
+	
+	public Object getNewValue() {
+		return _newValue;
+	}
+	
+	
+}
