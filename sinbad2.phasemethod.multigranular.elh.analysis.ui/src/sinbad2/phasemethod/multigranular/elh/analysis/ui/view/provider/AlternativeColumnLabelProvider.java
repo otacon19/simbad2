728df91927ba76496e7336148d14320f7e6d9465
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-02-17 11:07:59 +0100

    Creado plugin interfaz fase análisis de ELH

---
+++ sinbad2.phasemethod.multigranular.elh.analysis.ui/src/sinbad2/phasemethod/multigranular/elh/analysis/ui/view/provider/AlternativeColumnLabelProvider.java
@@ -0,0 +1,12 @@
+package sinbad2.phasemethod.multigranular.elh.analysis.ui.view.provider;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+
+public class AlternativeColumnLabelProvider extends ColumnLabelProvider {
+
+	@Override
+	public String getText(Object element) {
+		return (String) ((Object[]) element)[1];
+	}
+}
+
