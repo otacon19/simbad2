a61ec0565d9b0137601237a53e30d81692a8ffe2
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-02-17 12:14:35 +0100

    Añadidos nuevos tipos de agregación soportados en los métodos y creada
    la condición para que se puede seleccionar el método fusion de
    
    heterogeneous

--- sinbad2.method.linguistic.heterogeneous.fusion/src/sinbad2/method/linguistic/heterogeneous/fusion/Fusion.java
+++ sinbad2.method.linguistic.heterogeneous.fusion/src/sinbad2/method/linguistic/heterogeneous/fusion/Fusion.java
@@ -1,26 +1,30 @@
 package sinbad2.method.linguistic.heterogeneous.fusion;
 
-import java.util.List;
-
-import sinbad2.domain.Domain;
-import sinbad2.domain.DomainSet;
-import sinbad2.domain.DomainsManager;
-import sinbad2.domain.linguistic.fuzzy.FuzzySet;
+import sinbad2.element.ProblemElementsManager;
+import sinbad2.element.ProblemElementsSet;
+import sinbad2.element.alternative.Alternative;
+import sinbad2.element.criterion.Criterion;
+import sinbad2.element.expert.Expert;
 import sinbad2.method.MethodImplementation;
 import sinbad2.method.state.MethodStateChangeEvent;
+import sinbad2.valuation.valuationset.ValuationSet;
+import sinbad2.valuation.valuationset.ValuationSetManager;
 
 public class Fusion extends MethodImplementation {
 	
 	public static final String ID = "flintstones.method.linguistic.heterogeneous.fusion";
 	
-	private static final String EVALUATIONS_IN_NOT_BLTS_DOMAIN = "Evaluations in not BLTS domain";
-	private static final String EVALUATIONS_IN_NOT_LINGUISTIC_DOMAIN = "Evaluations in not linguistic domain";
+	private static final String NOT_SET_ALL_ASSIGNMENTS = "Not set all assignments";
 	
-	private DomainSet _domainsSet;
+	private ProblemElementsSet _elementsSet;
+	private ValuationSet _valuationSet;
 	
 	public Fusion() {
-		DomainsManager domainsManager = DomainsManager.getInstance();
-		_domainsSet = domainsManager.getActiveDomainSet();
+		ProblemElementsManager elementsManager = ProblemElementsManager.getInstance();
+		_elementsSet = elementsManager.getActiveElementSet();
+		
+		ValuationSetManager valuationSetManager = ValuationSetManager.getInstance();
+		_valuationSet = valuationSetManager.getActiveValuationSet();
 	}
 	
 	@Override
@@ -33,14 +37,13 @@ public class Fusion extends MethodImplementation {
 
 	@Override
 	public String isAvailable() {
-		List<Domain> domains = _domainsSet.getDomains();
-		for(Domain d: domains) {
-			if(d instanceof FuzzySet) {
-				if(!((FuzzySet) d).isBLTS()) {
-					return EVALUATIONS_IN_NOT_BLTS_DOMAIN;
+		for(Expert expert : _elementsSet.getExperts()) {
+			for(Criterion criterion : _elementsSet.getCriteria()) {
+				for(Alternative alternative : _elementsSet.getAlternatives()) {
+					if(_valuationSet.getValuation(expert, alternative, criterion) == null) {
+						return NOT_SET_ALL_ASSIGNMENTS;
+					}
 				}
-			} else {
-				return EVALUATIONS_IN_NOT_LINGUISTIC_DOMAIN;
 			}
 		}
 		return "";
