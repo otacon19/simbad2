496da80da93cdcddac13509e2b550287a9654ea4
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-02-23 12:18:32 +0100

    Creado plugin de interfaz para la fase de agregación 2-tuple

---
+++ sinbad2.phasemethod.linguistic.twotuple.aggregation.ui/src/sinbad2/phasemethod/linguistic/twotuple/aggregation/ui/view/editingsupport/OperatorWeightsEditingSupport.java
@@ -0,0 +1,202 @@
+package sinbad2.phasemethod.linguistic.twotuple.aggregation.ui.view.editingsupport;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.CheckboxCellEditor;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.widgets.Display;
+
+import sinbad2.aggregationoperator.AggregationOperator;
+import sinbad2.aggregationoperator.WeightedAggregationOperator;
+import sinbad2.element.ProblemElement;
+import sinbad2.element.ProblemElementsManager;
+import sinbad2.element.ProblemElementsSet;
+import sinbad2.element.criterion.Criterion;
+import sinbad2.element.expert.Expert;
+import sinbad2.phasemethod.linguistic.twotuple.aggregation.AggregationPhase;
+import sinbad2.phasemethod.linguistic.twotuple.aggregation.ui.view.dialog.QuantifiersDialog;
+import sinbad2.phasemethod.linguistic.twotuple.aggregation.ui.view.dialog.WeightsDialog;
+
+public class OperatorWeightsEditingSupport extends EditingSupport {
+
+	private TreeViewer _viewer;
+	private CheckboxCellEditor _cellEditor;
+	private AggregationPhase _aggregationPhase;
+	private String _type;
+
+	private static ProblemElement[] getLeafElements(ProblemElement root) {
+		ProblemElementsManager elementsManager = ProblemElementsManager.getInstance();
+		ProblemElementsSet elementsSet = elementsManager.getActiveElementSet();
+
+		List<ProblemElement> result = new LinkedList<ProblemElement>();
+		if (root instanceof Expert || root == null) {
+			List<Expert> childrens = elementsSet.getExpertChildren((Expert) root);
+			for (Expert children : childrens) {
+				if (!children.hasChildrens()) {
+					result.add(children);
+				} else {
+					ProblemElement[] subchildrens = getLeafElements(children);
+					for (ProblemElement subchildren : subchildrens) {
+						result.add(subchildren);
+					}
+				}
+			}
+
+		} else {
+			List<Criterion> subcriteria = elementsSet.getCriterionSubcriteria((Criterion) root);
+			for (Criterion subcriterion : subcriteria) {
+				if (!subcriterion.hasSubcriteria()) {
+					result.add(subcriterion);
+				} else {
+					ProblemElement[] sub2criteria = getLeafElements(subcriterion);
+					for (ProblemElement sub2criterion : sub2criteria) {
+						result.add(sub2criterion);
+					}
+				}
+			}
+
+		}
+
+		return result.toArray(new ProblemElement[0]);
+	}
+
+	public OperatorWeightsEditingSupport(TreeViewer viewer, String type, AggregationPhase aggregationPhase) {
+		super(viewer);
+
+		_aggregationPhase = aggregationPhase;
+		_type = type;
+		_viewer = viewer;
+	}
+
+	@Override
+	protected CellEditor getCellEditor(Object element) {
+		_cellEditor = new CheckboxCellEditor(_viewer.getTree());
+		return _cellEditor;
+
+	}
+
+	@Override
+	protected boolean canEdit(Object element) {
+
+		ProblemElement problemElement = null;
+		if (element instanceof ProblemElement) {
+			problemElement = (ProblemElement) element;
+		}
+
+		AggregationOperator operator;
+		if (AggregationPhase.EXPERTS.equals(_type)) {
+			operator = _aggregationPhase.getExpertOperator(problemElement);
+		} else {
+			operator = _aggregationPhase.getCriterionOperator(problemElement);
+		}
+		if (operator != null) {
+			if (operator instanceof WeightedAggregationOperator) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	protected Object getValue(Object element) {
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void setValue(Object element, Object value) {
+
+		if ((element == null) || (value == null)) {
+			return;
+		}
+
+		ProblemElement problemElement = null;
+		if (element instanceof ProblemElement) {
+			problemElement = (ProblemElement) element;
+		}
+
+		AggregationOperator operator;
+		if (AggregationPhase.EXPERTS.equals(_type)) {
+			operator = _aggregationPhase.getExpertOperator(problemElement);
+		} else {
+			operator = _aggregationPhase.getCriterionOperator(problemElement);
+		}
+
+		List<Double> weights = null;
+		Map<String, List<Double>> mapWeights = null;
+		Object aux = null;
+		if (AggregationPhase.EXPERTS.equals(_type)) {
+			aux = _aggregationPhase.getExpertOperatorWeights(problemElement);
+		} else {
+			aux = _aggregationPhase.getCriterionOperatorWeights(problemElement);
+		}
+		if (aux instanceof List<?>) {
+			weights = (List<Double>) aux;
+		} else if (aux instanceof Map<?, ?>) {
+			mapWeights = (Map<String, List<Double>>) aux;
+		}
+
+		String elementType;
+		String elementId;
+
+		if (AggregationPhase.EXPERTS.equals(_type)) {
+			elementType = "Expert";
+		} else {
+			elementType = "Criterion";
+		}
+
+		if (problemElement == null) {
+			if (AggregationPhase.EXPERTS.equals(_type)) {
+				elementId = "all experts";
+			} else {
+				elementId = "all criteria";
+			}
+		} else {
+			elementId = problemElement.getId();
+		}
+
+		if (operator.getName().equals("Owa")) { //$NON-NLS-1$
+			QuantifiersDialog dialog = new QuantifiersDialog(Display.getCurrent().getActiveShell(), weights.get(0), weights.get(1), QuantifiersDialog.SIMPLE, elementType, elementId);
+			if(dialog.open() == QuantifiersDialog.SAVE) {
+				weights = new LinkedList<Double>();
+				weights.add(dialog.getAlpha());
+				weights.add(dialog.getBeta());
+				if (AggregationPhase.EXPERTS.equals(_type)) {
+					_aggregationPhase.setExpertOperator(problemElement, operator, weights);
+				} else {
+					_aggregationPhase.setCriterionOperator(problemElement, operator, weights);
+
+				}
+			}
+		} else if(operator.getName().equals("Weighted mean")) { //$NON-NLS-1$
+
+			ProblemElement nullElement = null;
+			ProblemElement[] secondary = getLeafElements(nullElement);
+
+			ProblemElementsManager elementsManager = ProblemElementsManager.getInstance();
+			ProblemElementsSet elementsSet = elementsManager.getActiveElementSet();
+			
+			WeightsDialog dialog; 
+			if(elementType.equals("Expert")) {
+				dialog = new WeightsDialog(Display.getCurrent().getActiveShell(), elementsSet.getElementExpertChildren((Expert) problemElement), secondary, mapWeights, QuantifiersDialog.SIMPLE, elementType, elementId);
+			} else {
+				dialog = new WeightsDialog(Display.getCurrent().getActiveShell(), elementsSet.getElementCriterionSubcriteria((Criterion) problemElement), secondary, mapWeights, QuantifiersDialog.SIMPLE, elementType, elementId);
+			}
+			
+			if(dialog.open() == QuantifiersDialog.SAVE) {
+				mapWeights = dialog.getWeights();
+				if (AggregationPhase.EXPERTS.equals(_type)) {
+					_aggregationPhase.setExpertOperator(problemElement, operator, mapWeights);
+				} else {
+					_aggregationPhase.setCriterionOperator(problemElement, operator, mapWeights);
+				}
+			}
+		}
+	}
+}
+
