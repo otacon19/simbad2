7ca3544c7e24ad78780da55b7b9217c60946f8bb
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-02-23 12:35:38 +0100

    Creado plugin de interfaz fase de análisis 2-tuple

---
+++ sinbad2.phasemethod.linguistic.twotuple.analysis.ui/src/sinbad2/phasemethod/linguistic/twotuple/analysis/ui/view/provider/FilterLabelProvider.java
@@ -0,0 +1,15 @@
+package sinbad2.phasemethod.linguistic.twotuple.analysis.ui.view.provider;
+
+import org.eclipse.jface.viewers.LabelProvider;
+
+import sinbad2.element.ProblemElement;
+
+public class FilterLabelProvider extends LabelProvider {
+
+	@Override
+	public String getText(Object obj) {
+		String result = ((ProblemElement) obj).getId();
+		
+		return result;
+	}
+}
