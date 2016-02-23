9d12ff60546a9863e9996d0e5fafe6fbdcaaf98d
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-02-23 10:55:09 +0100

    Creado plugin fase de agregación 2-tuple

---
+++ sinbad2.phasemethod.linguistic.twotuple.aggregation/src/sinbad2/phasemethod/linguistic/twotuple/aggregation/listener/AggregationProcessListener.java
@@ -0,0 +1,8 @@
+package sinbad2.phasemethod.linguistic.twotuple.aggregation.listener;
+
+
+public interface AggregationProcessListener {
+	
+	public void notifyAggregationProcessChange(AggregationProcessStateChangeEvent event);		
+}
+
---
+++ sinbad2.phasemethod.linguistic.twotuple.aggregation/src/sinbad2/phasemethod/linguistic/twotuple/aggregation/listener/EAggregationProcessStateChange.java
@@ -0,0 +1,5 @@
+package sinbad2.phasemethod.linguistic.twotuple.aggregation.listener;
+
+public enum EAggregationProcessStateChange {
+	AGGREGATION_PROCESS_CHANGE;
+}
