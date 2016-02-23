287d9d73a88fea25d27308982f6765c00ffeff15
Álvaro Labella Romero <brokensword_19@hotmail.com>
2016-02-23 12:25:50 +0100

    Creado plugin fase de análisis 2-tuple

---
+++ sinbad2.phasemethod.linguistic.twotuple.analysis/src/sinbad2/phasemethod/linguistic/twotuple/analysis/AnalysisPhase.java
@@ -0,0 +1,57 @@
+package sinbad2.phasemethod.linguistic.twotuple.analysis;
+
+import sinbad2.phasemethod.IPhaseMethod;
+import sinbad2.phasemethod.listener.EPhaseMethodStateChange;
+import sinbad2.phasemethod.listener.PhaseMethodStateChangeEvent;
+
+public class AnalysisPhase implements IPhaseMethod {
+
+	public static final String ID = "flintstones.phasemethod.multigranular.fusion.analysis";
+	
+	private static AnalysisPhase _instance = null;
+	
+	public static AnalysisPhase getInstance() {
+		if(_instance == null) {
+			_instance = new AnalysisPhase();
+		}
+		return _instance;
+	}
+	
+	@Override
+	public IPhaseMethod copyStructure() {
+		return new AnalysisPhase();
+	}
+
+	@Override
+	public void copyData(IPhaseMethod iMethodPhase) {}
+
+	@Override
+	public void clear() {}
+
+	@Override
+	public void notifyPhaseMethodStateChange(PhaseMethodStateChangeEvent event) {
+		if (event.getChange().equals(EPhaseMethodStateChange.ACTIVATED)) {
+			activate();
+		}
+	}
+
+	@Override
+	public IPhaseMethod clone() {
+		AnalysisPhase result = null;
+
+		try {
+			result = (AnalysisPhase) super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public void activate() {}
+
+	@Override
+	public boolean validate() {
+		return true;
+	}
+}
