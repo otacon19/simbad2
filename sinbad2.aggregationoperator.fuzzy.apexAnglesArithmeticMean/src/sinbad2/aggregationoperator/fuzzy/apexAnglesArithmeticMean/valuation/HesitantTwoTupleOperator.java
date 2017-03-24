2567bfbeba34d81f59a2c6dded946154d8b0709e
Álvaro <lvaro@192.168.1.23>
2017-03-24 12:29:21 +0100

    Nuevo operador fuzzy
    1- Se ha añadido un nuevo operador fuzzy que calcula el número
    trapezoidal en función de ángulos y tangentes. El resultado es idéntico
    al de la media aritmética así que no sé si al final se mantendrá.

--- sinbad2.aggregationoperator.fuzzy.apexAnglesArithmeticMean/src/sinbad2/aggregationoperator/fuzzy/apexAnglesArithmeticMean/valuation/HesitantTwoTupleOperator.java
+++ sinbad2.aggregationoperator.fuzzy.apexAnglesArithmeticMean/src/sinbad2/aggregationoperator/fuzzy/apexAnglesArithmeticMean/valuation/HesitantTwoTupleOperator.java
@@ -1,4 +1,4 @@
-package sinbad2.aggregationoperator.fuzzy.arithmeticMean.valuation;
+package sinbad2.aggregationoperator.fuzzy.apexAnglesArithmeticMean.valuation;
 
 import java.util.List;
 
@@ -10,17 +10,53 @@ import sinbad2.valuation.hesitant.twoTuple.HesitantTwoTupleValuation;
 
 public class HesitantTwoTupleOperator {
 
-private HesitantTwoTupleOperator() {}
+	private HesitantTwoTupleOperator() {}
 	
 	public static Valuation aggregate(List<Valuation> valuations) {
 		HesitantTwoTupleValuation result = null;
 		double a = 0, b = 0, c = 0, d = 0;
 		double limits[] = new double[4];
-		FuzzySet domain = null;
 		int size = valuations.size();
 		
 		TrapezoidalFunction tpf;
+		
+		double[] angle = computeAngle(valuations);
+		
+		for(Valuation valuation : valuations) {
+			Validator.notIllegalElementType(valuation, new String[] { HesitantTwoTupleValuation.class.toString() });
 
+			tpf = ((HesitantTwoTupleValuation) valuation).getFuzzyNumber();
+				 
+			limits = tpf.getLimits();
+			
+			a += limits[1] - Math.tan(angle[0]);
+			b += limits[1];
+			c += limits[2];
+			d += limits[2] + Math.tan(angle[1]);
+		}
+		
+		a /= size;
+		b /= size;
+		c /= size;
+		d /= size;
+		
+		result = (HesitantTwoTupleValuation) valuations.get(0).clone();
+		result.createRelation(new TrapezoidalFunction(new double[]{a, b, c, d}));
+		
+		return result;
+	}
+
+	private static double[] computeAngle(List<Valuation> valuations) {
+		double[] angles = new double[2];
+		angles[0] = 0;
+		angles[1] = 0;
+		
+		FuzzySet domain = null;
+		int size = valuations.size();
+		double limits[] = new double[4];
+		
+		TrapezoidalFunction tpf;
+	
 		for(Valuation valuation : valuations) {
 			Validator.notIllegalElementType(valuation, new String[] { HesitantTwoTupleValuation.class.toString() });
 
@@ -37,21 +73,14 @@ private HesitantTwoTupleOperator() {}
 			}
 				 
 			limits = tpf.getLimits();
-			a += limits[0];
-			b += limits[1];
-			c += limits[2];
-			d += limits[3];
+			
+			angles[0] += Math.atan(limits[1] - limits[0]);
+			angles[1] += Math.atan(limits[3] - limits[2]);
 		}
-
-		a /= size;
-		b /= size;
-		c /= size;
-		d /= size;
 		
-		result = (HesitantTwoTupleValuation) valuations.get(0).clone();
-		result.createRelation(new TrapezoidalFunction(new double[]{a, b, c, d}));
+		angles[0] /= size;
+		angles[1] /= size;
 		
-
-		return result;
+		return angles;
 	}
 }
